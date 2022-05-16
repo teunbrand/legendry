fc6cfba98800b39451b45b292ae14dbb9821cbe5
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2022-05-16 22:55:26 +0200

    Parent of guides

---
+++ R/gguidance-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import ggplot2
+#' @import grid
+#' @import rlang
+NULL
+
+## usethis namespace: start
+#' @importFrom glue glue
+#' @importFrom glue glue_collapse
+#' @importFrom gtable gtable_col
+#' @importFrom gtable gtable_height
+#' @importFrom gtable gtable_row
+#' @importFrom gtable gtable_width
+#' @importFrom stats setNames
+#' @importFrom vctrs new_data_frame
+#' @importFrom vctrs vec_assert
+## usethis namespace: end
+NULL
