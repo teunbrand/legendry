a24972fea4f98a5bc00a62901ae286b8344bdb54
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2023-07-02 00:32:47 +0200

    Wrap major breaks too

--- R/utils-scale.R
+++ R/utils-scale.R
@@ -32,7 +32,8 @@ scale_grid_extract <- function(major = waiver(), minor = waiver(), scale) {
       NULL, scale$scale,
       breaks = major, minor_breaks = minor,
       limits = scale$continuous_range,
-      get_breaks_minor = minor_no_discard
+      get_breaks_minor = minor_no_discard,
+      get_breaks = major_early_exit
     )
 
     major <- new_scale$get_breaks() %|W|% default_major
@@ -82,3 +83,54 @@ minor_no_discard <- function(self, n = 2, b = self$break_position(),
   }
   breaks
 }
+
+# Exits early if break function returns waiver
+major_early_exit = function(self, limits = self$get_limits()) {
+
+  if (self$is_empty()) {
+    return(numeric())
+  }
+  domain <- suppressWarnings(self$trans$transform(self$trans$domain))
+  domain <- sort(domain)
+  if (length(domain) == 2 && !scales::zero_range(domain)) {
+    limits <- scales::oob_squish(limits, domain)
+  }
+  limits <- self$trans$inverse(limits)
+  if (is.null(self$breaks)) {
+    return(NULL)
+  }
+  if (identical(self$breaks, NA)) {
+    cli::cli_abort(
+      "Invalid {.arg breaks} specification. Uses {.val NULL}, not {.val NA}."
+    )
+  }
+  if (scales::zero_range(as.numeric(limits))) {
+    breaks <- limits[1]
+  } else if (inherits(self$breaks, "waiver")) {
+    if (!is.null(self$n.breaks) &&
+        ("n" %in% names(formals(self$trans$breaks)))) {
+      breaks <- self$trans$breaks(limits, self$n.breaks)
+    } else {
+      if (!is.null(self$n.breaks)) {
+        cli::cli_warn(paste0(
+          "Ignoring {.arg n.breaks}. Use a {.cls trans} object that supports ",
+          "setting number of breaks."
+        ))
+      }
+      breaks <- self$trans$breaks(limits)
+    }
+  } else if (is.function(self$breaks)) {
+    breaks <- self$breaks(limits)
+    if (inherits(breaks, "waiver")) {
+      return(breaks)
+    }
+  } else {
+    breaks <- self$breaks
+  }
+
+  breaks <- self$trans$transform(breaks)
+  breaks <- scales::censor(breaks, self$trans$transform(limits),
+                           only.finite = FALSE)
+  breaks
+}
+
