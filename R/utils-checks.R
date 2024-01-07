3cdf192950efa286c09d84fb3016398119238d29
Teun van den Brand <tahvdbrand@gmail.com>
2024-01-07 13:09:14 +0100

    more checks

--- R/utils-checks.R
+++ R/utils-checks.R
@@ -12,6 +12,22 @@ check_columns <- function(data, columns, call = caller_env(),
   )
 }
 
+check_unit <- function(x, allow_null = FALSE, call = caller_env(),
+                       arg = caller_arg(x)) {
+  if (!missing(x)) {
+    if (is.unit(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+  }
+  stop_input_type(
+    x, as_cli("a {.cls unit} object"),
+    allow_null = allow_null, arg = arg, call = call
+  )
+}
+
 check_object <- function(
   x, check_fun, what, ...,
   allow_null = FALSE, arg = caller_arg(x), call = caller_env()
@@ -28,6 +44,50 @@ check_object <- function(
     x, as_cli(what), ...,
     allow_null = allow_null, arg = arg, call = call
   )
+}
+
+check_length <- function(
+  x, min = NULL, max = NULL, exact = NULL,
+  allow_null = FALSE, arg = caller_arg(x), call = caller_env()
+) {
+  if (!missing(x)) {
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+    length <- length(x)
+    if (!is.null(min %||% max)) {
+      between <- c(min %||% 0, max %||% Inf)
+      if (length >= between[1] && length <= between[2]) {
+        return(invisible(NULL))
+      }
+    }
+    if (!is.null(exact) && length %in% exact) {
+      return(invisible(NULL))
+    }
+  }
+  if (is.null(min) && is.null(max) && is.null(exact)) {
+    return(invisible(NULL))
+  }
+  if (!is.null(min) && !is.null(max)) {
+    what <- "a vector with {.field length} between {min} and {max}"
+  } else if (!is.null(min)) {
+    what <- "a vector with {.field length} more than or equal to {min}"
+  } else if (!is.null(max)) {
+    if (max > 1) {
+      what <- "a vector with {.field length} less than or equal to {max}"
+    } else {
+      what <- "a value with {.field length} less than or equal to {max}"
+    }
+  } else if (!is.null(exact)) {
+    if (exact > 1) {
+      what <- "a vector with {.field length} equal to {exact}"
+    } else {
+      what <- "a single value with {.field length} equal to {exact}"
+    }
+  }
+  obj <- obj_type_friendly(x, value = FALSE)
+  msg <- "{.arg {arg}} must be {as_cli(what)}, not {obj} with length {length(x)}."
+  cli::cli_abort(msg, call = call)
 }
 
 check_inherits <- function(
