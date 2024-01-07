7b15d3ce1562a0a520f502c6517ce617b52116b0
Teun van den Brand <tahvdbrand@gmail.com>
2024-01-07 22:49:00 +0100

    new sandwich composition

---
+++ R/compose-sandwich.R
@@ -0,0 +1,106 @@
+#' Compose guides as a sandwich
+#'
+#' This guide composition has a middle guide flanked by two parallel guides.
+#'
+#' @param key A [standard key][key_standard] specification. The key is shared
+#'   among all guides that have `NULL` keys themselves. See more information
+#'   in the linked topic.
+#' @param middle Guide to use as the middle guide. Each guide can be specified
+#'   as one of the following:
+#'   * A `<Guide>` class object.
+#'   * A `<function>` that returns a `<Guide>` class object.
+#'   * A `<character>` naming such a function, without the `guide_` or
+#'   `primitive_` prefix.
+#' @param text,opposite Guides to use at the `legend.text.position` location
+#'   and on the opposite side of the `middle` guide respectively. Guide
+#'   specification is the same as in the `middle` argument.
+#' @inheritParams compose_crux
+#'
+#' @return A `<ComposeSandwich>` guide object.
+#' @export
+#' @family composition
+#'
+#' @examples
+#' # A standard plot with a sandwich guide
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point(aes(colour = cty)) +
+#'   guides(colour = compose_sandwich(
+#'     middle = "colourbar",
+#'     text = "axis_custom",
+#'     opposite = primitive_bracket(key = key_range_manual(
+#'       start = c(10, 20), end = c(25, 30), name = c("A", "B")
+#'     ))
+#'   ))
+compose_sandwich <- function(
+  key = key_auto(),
+  middle = gizmo_barcap(),
+  text = "none",
+  opposite = "none",
+  args = list(),
+  complete = FALSE,
+  theme = NULL,
+  theme_defaults = list(),
+  reverse = FALSE,
+  order = 0,
+  title = waiver(),
+  position = waiver(),
+  available_aes = NULL
+) {
+  new_compose(
+    guides = list(
+      middle = middle,
+      text = text,
+      opposite = opposite
+    ),
+    title = title,
+    key = key,
+    args = args,
+    complete = complete,
+    reverse = reverse,
+    available_aes = available_aes,
+    order = order,
+    theme_defaults = theme_defaults,
+    position = position,
+    name = "sandwich_composition",
+    super = ComposeSandwich
+  )
+}
+
+# Class -------------------------------------------------------------------
+
+#' @export
+#' @rdname gguidance_extensions
+#' @format NULL
+#' @usage NULL
+ComposeSandwich <- ggproto(
+  "ComposeSandwich", Compose,
+
+  params = c(Compose$params, list(complete = FALSE, theme_defaults = list(),
+                                  reverse = FALSE)),
+
+  draw = function(self, theme, position = NULL, direction = NULL,
+                  params = self$params) {
+
+    direction <- params$direction %||% direction
+    text_position <- calc_element("legend.text.position", theme) %||%
+      switch(direction, horizontal = "bottom", vertical = "right")
+
+    valid_position <- switch(
+      direction, horizontal = c("bottom", "top"), vertical = c("left", "right")
+    )
+    check_position(text_position, valid_position, arg = "legend.text.position")
+
+    theme <- theme + params$theme
+    theme <- replace_null(theme, !!!params$theme_defaults)
+
+    opposite <- opposite_position(text_position)
+    params$guide_params$opposite$draw_label <- FALSE
+
+    old <- c("middle", "text", "opposite")
+    new <- c("centre", text_position, opposite)
+    params$guides <- rename(params$guides, old, new)
+    params$guide_params <- rename(params$guide_params, old, new)
+
+    ComposeCrux$draw(theme, position, direction, params)
+  }
+)
---
+++ tests/testthat/test-compose_sandwich.R
@@ -0,0 +1,45 @@
+test_that("compose_sandwich can compose a legend", {
+
+  sandwich <- compose_sandwich(
+    middle = guide_colourbar(theme = theme(text = element_text(colour = "limegreen"))),
+    text   = guide_axis_custom(theme = theme(text = element_text(colour = "tomato"))),
+    opposite = primitive_bracket(key = key_range_manual(
+      c(10, 20), c(25, 30), c("A", "B")
+    ), theme = theme(text = element_text(colour = "dodgerblue")))
+  )
+
+
+  p <- ggplot(mpg, aes(displ, hwy)) +
+    geom_point(aes(colour = cty)) +
+    scale_colour_viridis_c(
+      guide = sandwich
+    )
+
+  vdiffr::expect_doppelganger(
+    "vertical sandwich", p + theme(
+      legend.text.position = "right",
+      legend.position = "right"
+    )
+  )
+
+  vdiffr::expect_doppelganger(
+    "vertical sandwich flipped", p + theme(
+      legend.text.position = "left",
+      legend.position = "right"
+    )
+  )
+
+  vdiffr::expect_doppelganger(
+    "horizontal sandwich", p + theme(
+      legend.text.position = "bottom",
+      legend.position = "bottom"
+    )
+  )
+
+  vdiffr::expect_doppelganger(
+    "horizontal sandwich flipped", p + theme(
+      legend.text.position = "top",
+      legend.position = "bottom"
+    )
+  )
+})
