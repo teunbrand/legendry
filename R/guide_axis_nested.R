10b581f4ff2f14b87791abca6aa57e2083a87a8d
Teun van den Brand <tahvdbrand@gmail.com>
2024-09-11 21:30:09 +0200

    document

--- R/guide_axis_nested.R
+++ R/guide_axis_nested.R
@@ -1,3 +1,74 @@
+# Constructor -------------------------------------------------------------
+
+#' Nested axis guide
+#'
+#' This axis guide gives extra range annotations to position scales. It can
+#' be used to infer nesting structure from labels or annotate ranges.
+#'
+#' @param key A [range key][key_range] specification. If not
+#'   `key = "range_auto"`, additional labels will be inserted to represent
+#'   point values.
+#' @param regular_key A [standard key][key_standard] specification for the
+#'   appearance of regular tick marks.
+#' @param type Appearance of ranges, either `"box"` to put text in boxes or
+#'   `"bracket"` (default) to text brackets.
+#' @inheritParams common_parameters
+#' @inheritParams primitive_line
+#' @inheritParams primitive_ticks
+#' @inheritParams primitive_bracket
+#' @param ... Arguments passed on to [`primitive_bracket()`] or
+#'   [`primitive_box()`].
+#'
+#' @details
+#' Under the hood, this guide is a [stack composition][compose_stack] of a
+#' [line][primitive_line], [ticks][primitive_ticks], optionally
+#' [labels][primitive_labels] and either [bracket][primitive_bracket] or
+#' [box][primitive_box] primitives.
+#'
+#' By default, the [`key = "range_auto"`][key_range] will incorporate the 0th
+#' level labels inferred from the scale's labels. These labels will look like
+#' regular labels.
+#'
+#' To offer other keys the opportunity to display ranges alongside
+#' regular-looking labels, the `regular_key` argument can be used to setup a
+#' separate key for display in between the ticks and ranges.
+#'
+#' @return A `<Guide>` object.
+#' @export
+#' @family standalone guides
+#'
+#' @examples
+#' # A plot with nested categories on the x-axis
+#' p <- ggplot(mpg, aes(interaction(drv, cyl), hwy)) +
+#'   geom_boxplot()
+#'
+#' p + guides(x = "axis_nested")
+#'
+#' # Apply styling to brackets
+#' p + guides(x = "axis_nested") +
+#'   theme_guide(bracket = element_line("red", linewidth = 1))
+#'
+#' # Don't drop nesting indicators that have 0-width
+#' p + guides(x = guide_axis_nested(drop_zero = FALSE))
+#'
+#' # Change additional padding for discrete categories
+#' p + guides(x = guide_axis_nested(pad_discrete = 0))
+#'
+#' # Change bracket type
+#' p + guides(x = guide_axis_nested(bracket = "curvy"))
+#'
+#' # Use boxes instead of brackets + styling of boxes
+#' p + guides(x = guide_axis_nested(type = "box")) +
+#'   theme_guide(box = element_rect("limegreen", "forestgreen"))
+#'
+#' # Use as annotation of a typical axis
+#' # `regular_key` controls display of typical axis
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point() +
+#'   guides(x = guide_axis_nested(
+#'     key = key_range_manual(start = 2:3, end = 5:6, name = c("First", "Second")),
+#'     regular_key = key_manual(c(2, 2.5, 3, 5, 7))
+#'   ))
 guide_axis_nested <- function(
   key   = "range_auto",
   regular_key = "auto",
