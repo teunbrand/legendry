27927c50d80b7a1e433e00e46d77da7f6c8eab59
Teun van den Brand <tahvdbrand@gmail.com>
2025-06-09 22:38:51 +0200

    :bug: add utils namespace

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
 on_load(
   if ("class_mapping" %in% getNamespaceExports("ggplot2")) {
-    class_mapping <- getFromNamespace("class_mapping", "ggplot2")
+    class_mapping <- utils::getFromNamespace("class_mapping", "ggplot2")
     new_aes <- function(x, env = caller_env()) {
       class_mapping(x, env = env)
     }
