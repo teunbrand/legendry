35cab0230f817d3e5cb3b399759bf50ff29a0c71
Teun van den Brand <tahvdbrand@gmail.com>
2025-09-28 17:48:16 +0200

    write documentation

--- R/guide-legend-manual.R
+++ R/guide-legend-manual.R
@@ -1,9 +1,83 @@
+# Constructor -------------------------------------------------------------
 
+#' Manual legend
+#'
+#' This is a guide that displays user-defined keys independent of scales.
+#' It should only be used as a last resort when struggling to format a
+#' conventional legend.
+#'
+#' @param labels
+#' Labels to display next to the keys. Can be a `<character>` or `<expression>`
+#' vector to set labels, or `NULL` to draw no labels.
+#' @param ...
+#' Arguments interpreted as aesthetics. For example: `colour = "red"`.
+#' The aesthetics must have the same size as the `labels` argument, or have
+#' size 1. These aesthetics may be overruled by fixed aesthetics set in the
+#' `layers` argument.
+#' @param layers
+#' A `<list>` of layers (`<LayerInstance>` objects) created by the `geom_*()` or
+#' `stat_*()` family of functions. These layers are used for their
+#' [`key_glyph`][ggplot2::draw_key] drawing functions, as well as to populate
+#' default aesthetics. Any fixed aesthetics provided to these layers overrule
+#' aesthetics passed to the `...` argument.
+#' @param title
+#' One of the following to indicate the title of the guide:
+#' * A `<character[1]>` or `<expression[1]>` to set a custom title.
+#' * `NULL` to not display any title.
+#' @param legend_args
+#' A `<list>` of arguments passed on to [`guide_legend_base()`].
+#'
+#' @details
+#' Because this guide is not tied to a scale, it can be given an arbitrary name
+#' in `guides()`; as long as it doesn't clash with other aesthetics.
+#'
+#'
+#' @returns A `<GuideCustom>` object.
+#' @export
+#' @family standalone guides
+#' @family legend guides
+#'
+#' @examples
+#' # A standard plot
+#' p <- ggplot(mtcars, aes(disp, mpg)) +
+#'   geom_point()
+#'
+#' # Typical usage: set `label` and some aesthetics
+#' p + guides(
+#'   some_name = guide_legend_manual(
+#'     label  = c("foo", "bar"),
+#'     colour = c(NA, "black"),
+#'     fill   = c("grey40", NA),
+#'     layers = geom_col()
+#'   )
+#' )
+#'
+#' # Alternative: use `layers` to set aesthetics
+#' p + guides(
+#'   some_name = guide_legend_manual(
+#'     label = c("foo", "bar"),
+#'     layers = geom_col(
+#'       # Must match length of `label`
+#'       colour = c(NA, "black"),
+#'       fill = c("grey40", NA)
+#'     )
+#'   )
+#' )
+#'
+#' # You can use >1 layer
+#' p + guides(
+#'   some_name = guide_legend_manual(
+#'     label = c("foo", "bar"),
+#'     colour = c("tomato", "dodgerblue"),
+#'     fill = NA,
+#'     layers = list(geom_col(), geom_point())
+#'   )
+#' )
 guide_legend_manual <- function(
   labels,
   ...,
   layers = list(geom_point()),
-  title = waiver(),
+  title = NULL,
   legend_args = list()
 ) {
   labels <- label_as_vector(labels)
@@ -38,8 +112,12 @@ guide_legend_manual <- function(
   )
 }
 
+# Class -------------------------------------------------------------------
 
-
+#' @export
+#' @rdname legendry_extensions
+#' @format NULL
+#' @usage NULL
 GuideLegendManual <- ggproto(
   "GuideLegendManual", GuideCustom,
 
