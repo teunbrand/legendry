bb23566f97b32873b2cf06b9a441692c7f10efd0
Teun van den Brand <tahvdbrand@gmail.com>
2026-05-09 19:27:21 +0200

    isolate ontop assembly

--- R/compose-ontop.R
+++ R/compose-ontop.R
@@ -67,8 +67,8 @@ ComposeOntop <- ggproto(
                   params = self$params) {
     theme <- theme + params$theme
 
-    position  <- params$position  %||% position
-    direction <- params$direction %||% direction
+    params$position  <- position  <- params$position  %||% position
+    params$direction <- direction <- params$direction %||% direction
 
     n_guides <- length(params$guides)
     guide_index <- seq_len(n_guides)
@@ -109,33 +109,42 @@ ComposeOntop <- ggproto(
       )
     }
 
-    keep  <- !map_lgl(grobs, is_zero)
+    self$assemble_drawing(grobs, params = params)
+  },
+
+  assemble_drawing = function(grobs, layout = NULL, sizes = NULL, params, elements = NULL) {
+    keep <- !map_lgl(grobs, is_zero)
     grobs <- grobs[keep]
     if (length(grobs) == 0L) {
       return(zeroGrob())
     }
 
-    origin <- unit(as.numeric(position %in% c("left", "bottom")), "npc")
-    just   <- opposite_position(position)
-    along  <- seq_along(grobs)
-    widths <- width_cm(grobs)
+    position <- params$position
+    anchor <- unit(as.numeric(position %in% c("left", "bottom")), "npc")
+    just <- opposite_position(position)
+
+    widths  <- width_cm(grobs)
     heights <- height_cm(grobs)
-    names <- paste0("guide-ontop-", along)
+
+    index <- seq_along(grobs)
+    names <- paste0("guide-ontop", index)
 
     if (position %in% c("bottom", "top")) {
       height <- unit(max(heights), "cm")
-      gt <- gtable(widths = unit(1.0, "npc"), heights = height)
-      gt <- gtable_add_grob(gt, grobs, t = 1L, l = 1L, name = names, clip = "off")
-      vp <- viewport(y = origin, height = height, just = just)
+      gt <- gtable(widths = unit(1.0, "npc"), heights = height) |>
+        gtable_add_grob(grobs, t = 1L, l = 1L, name = names, clip = "off")
+      vp <- viewport(y = anchor, height = height, just = just)
     } else {
       width <- unit(max(widths), "cm")
-      gt <- gtable(widths = width, heights = unit(1.0, "npc"))
-      gt <- gtable_add_grob(gt, grobs, t = 1L, l = 1L, name = names, clip = "off")
-      vp <- viewport(x = origin, width = width, just = just)
+      gt <- gtable(widths = width, heights = unit(1.0, "npc")) |>
+        gtable_add_grob(grobs, t = 1L, l = 1L, name = names, clip = "off")
+      vp <- viewport(x = anchor, width = width, just = just)
     }
+
     absoluteGrob(
-      grob = gList(gt), vp = vp,
-      width = gtable_width(gt), height = gtable_height(gt)
+      grob   = gList(gt), vp = vp,
+      width  = gtable_width(gt),
+      height = gtable_height(gt)
     )
   }
 )
