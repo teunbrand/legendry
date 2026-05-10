90b4ffa3c7217c7fe1940cb2f4c980fc4c3acda2
Teun van den Brand <tahvdbrand@gmail.com>
2026-05-10 12:20:24 +0200

    key properties for `primitive_box()`

--- R/primitive-box.R
+++ R/primitive-box.R
@@ -118,16 +118,12 @@ PrimitiveBox <- ggproto(
   extract_params = extract_range_params,
 
   extract_decor = function(scale, aesthetic, key, ...) {
-
     key <- vec_slice(key, key$.draw)
-    n_keys <- nrow(key)
-    value <- vec_interleave(key$start, key$end)
-
-    data_frame0(
-      !!aesthetic := value,
-      group  = rep(seq_len(n_keys), each = 2L),
-      .level = rep(key$.level, each = 2L)
-    )
+    decor <- key[setdiff(names(key), c("start", "end"))]
+    decor$group <- seq_len(nrow(key))
+    decor <- vec_rep_each(decor, 2)
+    decor[[aesthetic]] <- vec_interleave(key$start, key$end)
+    decor
   },
 
   transform = function(self, params, coord, panel_params) {
@@ -241,8 +237,9 @@ draw_box <- function(decor, element, size, offset, position) {
     return(zeroGrob())
   }
   aes <- switch(position, top = , bottom = "x", left = , right = "y", "theta")
-
   rle <- new_rle(decor$group)
+  props <- element_key_properties(vec_slice(decor, rle$start), "rect")
+
   if (is_theta(position)) {
     rev <- vec_slice(decor, rev(vec_seq_along(decor)))
     x <- unit(c(decor$x, rev$x), "npc")
@@ -252,17 +249,18 @@ draw_box <- function(decor, element, size, offset, position) {
     x <- x + unit(sin(theta) * offset, "cm")
     y <- y + unit(cos(theta) * offset, "cm")
     id <- c(decor$group, rev$group)
-    gp <- gpar(
-      col = element$colour,
-      fill = element$fill,
-      lwd = (element$linewidth * .pt) %0% NULL,
-      lty = (element$linetype)
+    element <- destructure_element(element)
+    gp <- gg_par(
+      col  = props$colour    %||% element$colour,
+      fill = props$fill      %||% element$fill,
+      lwd  = props$linewidth %||% element$linewidth,
+      lty  = props$linetype  %||% element$linetype,
+      linejoin = element$linejoin
     )
     grob <- polygonGrob(x = x, y = y, id = id, gp = gp)
     return(grob)
   }
 
-  rle <- new_rle(decor$group)
   start <- decor[[aes]][rle$start]
   end   <- decor[[aes]][rle$end]
   min <- pmin(start, end)
@@ -275,5 +273,5 @@ draw_box <- function(decor, element, size, offset, position) {
   if (position %in% c("left", "right")) {
     args <- flip_names(args)
   }
-  inject(element_grob(element, !!!args))
+  inject(element_grob(element, !!!args, !!!props))
 }
