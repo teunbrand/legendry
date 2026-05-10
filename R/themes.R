b0ea4fbbe4ea54b4ce1046f449d15636088043b5
Teun van den Brand <tahvdbrand@gmail.com>
2026-05-10 23:37:22 +0200

    Include newer elements in `theme_guide()`

--- R/themes.R
+++ R/themes.R
@@ -1,10 +1,18 @@
 
 #' Theme wrapper for guides
 #'
-#' This function has shorthand names for theme elements relating to guides. It
-#' is intended to be used as the `guide_*(theme)` argument. Because of this
-#' intent, and due to legends and axes having mutually exclusive theme elements,
-#' this function sets the elements for both simultaneously.
+#' This function does three things different than [`theme()`].
+#'
+#' 1. It has shorthand names for various guide settings, similar to
+#'   [`theme_sub_legend()`] and [`theme_sub_axis()`]
+#' 2. It simultaneously sets theme elements for axes and legends. This makes
+#'   it contextually agnostic. For example, setting `theme_guide(text)` will
+#'   populate `legend.text` and also `axis.text`.
+#' 3. It includes \pkg{legendry} specific settings.
+#'
+#' The first two things make it very good for setting the `guide_*(theme)`
+#' arguments. The second thing makes it very bad for setting plot-wide or
+#' global themes.
 #'
 #' @param text An [`<element_text>`][ggplot2::element_text] setting both
 #'   `legend.text` and `axis.text` elements.
@@ -36,8 +44,9 @@
 #'   `legendry.legend.minor.ticks.length`.
 #'   * `mini.ticks.length` sets both `legendry.axis.mini.ticks.length` and
 #'   `legendry.legend.mini.ticks.length`.
-#' @param spacing,group.spacing A [`<unit[1]>`][grid::unit()] setting both the
-#'   `legendry.guide.spacing` and `legendry.group.spacing` theme elements.
+#' @param spacing,group.spacing,table.spacing A [`<unit[1]>`][grid::unit()]
+#'   settingthe `legendry.guide.spacing`, `legendry.group.spacing` and
+#'   `legendry.table.spacing` theme elements respectively.
 #' @param key An [`<element_rect>`][ggplot2::element_rect] setting the
 #'   `legend.key` element.
 #' @param key.size,key.width,key.height A [`<unit>`][grid::unit()] setting the
@@ -48,6 +57,8 @@
 #'   `legend.key.spacing.y` elements respectively.
 #' @param key.margin A [`<margin>`][ggplot2::margin] setting the margin around
 #'   legend glyphs.
+#' @param key.justification A [`<numeric[2]>`] passed to the
+#'   `legend.key.justification` setting.
 #' @param frame An [`<element_rect>`][ggplot2::element_rect] setting the
 #'   `legend.frame` element.
 #' @param byrow A `<logical[1]>` setting the `legend.byrow` element.
@@ -64,6 +75,13 @@
 #' @param fence,fence.post,fence.rail An
 #'   [`<element_line>`][ggplot2::element_line] setting the `legendry.fence`,
 #'   `legendry.fence.post` and `legendry.fence.rail` respectively.
+#' @param zebra.light,zebra.dark An [`<element_rect>`][ggplot2::element_rect]
+#'   setting the `legendry.zebra.light` and `legendry.zebra.dark` settings
+#'   respectively.
+#' @param point An [`<element_point>`][ggplot2::element_point] setting the
+#'   `legendry.point` element.
+#' @param connector An [`<elemenet_line>`][ggplot2::element_line] setting the
+#'   `legendry.connector` element.
 #'
 #' @return A `<theme>` object that can be provided to a guide.
 #' @export
@@ -101,6 +119,7 @@ theme_guide <- function(
 
   spacing = NULL,
   group.spacing = NULL,
+  table.spacing = NULL,
 
   key = NULL,
   key.size = NULL,
@@ -110,6 +129,7 @@ theme_guide <- function(
   key.spacing.x = NULL,
   key.spacing.y = NULL,
   key.margin = NULL,
+  key.justification = NULL,
 
   frame = NULL,
   byrow = NULL,
@@ -121,7 +141,11 @@ theme_guide <- function(
   box = NULL,
   fence = NULL,
   fence.post = NULL,
-  fence.rail = NULL
+  fence.rail = NULL,
+  zebra.light = NULL,
+  zebra.dark = NULL,
+  point = NULL,
+  connector = NULL
 ) {
 
   theme <- list(
@@ -168,6 +192,7 @@ theme_guide <- function(
 
     legendry.guide.spacing = spacing,
     legendry.group.spacing = group.spacing,
+    legendry.table.spacing = table.spacing,
 
     legend.key = key,
     legend.key.spacing = key.spacing,
@@ -176,6 +201,7 @@ theme_guide <- function(
     legend.key.size = key.size,
     legend.key.width = key.width,
     legend.key.height = key.height,
+    legend.key.justification = key.justification,
     legendry.legend.key.margin = key.margin,
 
     legend.frame = frame,
@@ -188,7 +214,11 @@ theme_guide <- function(
     legendry.box = box,
     legendry.fence = fence,
     legendry.fence.post = fence.post,
-    legendry.fence.rail = fence.rail
+    legendry.fence.rail = fence.rail,
+    legendry.zebra.light = zebra.light,
+    legendry.zebra.dark = zebra.dark,
+    legendry.point = point,
+    legendry.connector = connector
   )
   theme <- theme[lengths(theme) > 0L]
   theme(!!!theme)
